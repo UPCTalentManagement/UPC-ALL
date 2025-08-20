73a4cbb495ec40bfb997cd3279eba13817ad0b23
UPCTalentManagement <mahmoudhanwerfahmy@gmail.com>
2025-08-20 11:24:07 +0300

    Add files via upload

---
+++ ramadan/story_content/triggers.js
@@ -0,0 +1,72 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6pMs0qmzcvd":
+        Script1();
+        break;
+      case "5zdFm9Ascfc":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('5isngQtongI');
+const duration = 500;
+const easing = 'ease-out';
+const id = '6Oc8ylbC4Iv';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5hjWjHje0An');
+const duration = 500;
+const easing = 'ease-out';
+const id = '5rx0KzReyH1';
+const teeterAmount = 4;
+const signs = ['-', '', '-'];
+
+player.addForTriggers(
+id,
+target.animate([
+{ rotate: '0deg' },
+{ rotate: `${signs[0] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[1] + teeterAmount}deg` },
+{ rotate: '0deg' },
+{ rotate: `${signs[2] + teeterAmount}deg` },
+{ rotate: '0deg' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
